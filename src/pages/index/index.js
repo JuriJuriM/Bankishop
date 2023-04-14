83564a6947dbcf079bfb840acd789dbc416e8efa
JuriJuriM <jurimudry@gmail.com>
2023-04-14 17:39:40 +0300

    add js

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,7 @@
 import './index.scss';
 
+// menu burger
+
 const headerBurger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
 const body = document.querySelector('body');
@@ -11,3 +13,36 @@ function handleClick() {
 }
 
 headerBurger.addEventListener('click', handleClick);
+
+// searching of picture with input 
+
+const headerInput = document.querySelector('.header__input');
+
+headerInput.oninput = function () {
+  let val = this.value.trim();
+  let subtitleItems = document.querySelectorAll('.first__subtitle');
+  let contentPic = document.querySelectorAll('.first__content');
+  if (val != '') {
+    subtitleItems.forEach(function (elem) {
+      if (elem.innerText.search(val) == -1) {
+        elem.classList.add('hide');
+        elem.innerHTML = elem.innerText;
+      }
+      else {
+        elem.classList.remove('hide');
+        let str = elem.innerText;
+        elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length);
+      }
+    });
+  }
+  else {
+    subtitleItems.forEach(function (elem) {
+      elem.classList.remove('hide');
+      elem.innerHTML = elem.innerText;
+    });
+  }
+};
+
+function insertMark(string, pos, len) {
+  return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len);
+}
